ef905d5099c50d9f2dea064274aafc3a3fbcbb79
Boberg <1823919+blazed@users.noreply.github.com>
2026-08-27 20:44:10 +0200

    build: add devenv development environment
    Signed-off-by: Boberg <1823919+blazed@users.noreply.github.com>

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  ...
+}:
+{
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    components = [
+      "cargo"
+      "clippy"
+      "llvm-tools-preview"
+      "rust-analyzer"
+      "rustc"
+      "rustfmt"
+    ];
+  };
+
+  languages.nix.enable = true;
+
+  packages = with pkgs; [
+    actionlint
+    cargo-deny
+    grpcurl
+    nixfmt
+    postgresql
+    python3
+    ruff
+    shellcheck
+    shfmt
+    sqlx-cli
+  ];
+
+  enterShell = ''
+    echo "Terrarium"
+  '';
+}
